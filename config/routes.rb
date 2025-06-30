1e964fb8871e61fe3fe52e93ac4470a16e11ccc0
satohirok <hiroki_g0421@yahoo.co.jp>
2025-07-01 07:20:15 +0900

    feat: トップページのコントローラー、ヘルパー、ビュー、ルーティング、テストを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # get 'top/index'
   resources :tasks
+  root 'top#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
